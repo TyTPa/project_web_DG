b15083a88f137ac89b5602f9c83d0f82087b474b
TyTPa <papushina.tatyana@gmail.com>
2025-04-13 20:25:35 +0300

    Первые шаги с Django 2 урок

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.index),
-    path('data/',views.data),
-    path ('test/',views.test)
+    path('', views.index, name ='home'),
+    path('data/',views.data, name = 'data'),
+    path ('test/',views.test, name = 'test'),
+    path ('new/',views.new, name ='page2')
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    data = {
+        'caption': "CatDjango"
+    }
+    return render(request, 'main/index.html', data)
+def new(request):
+    return render(request, 'main/new.html')
 def data(request):
     return HttpResponse("<h1>Это моя вторая страница Data</h1>")
 def test(request):
